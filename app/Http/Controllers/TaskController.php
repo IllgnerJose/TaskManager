d293ea48ef0ec8c332ffb23f5009ec7d0b3f4109
IllgnerJose <illgnerjosbatista@gmail.com>
2025-02-22 14:48:49 -0300

    feat: implement create function

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,24 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Services\TaskService;
+use App\Http\Requests\StoreTaskRequest;
 
 class TaskController extends Controller
 {
+    protected $taskService;
+
+    public function __construct(TaskService $taskService)
+    {
+        $this->taskService = $taskService;
+    }
+
     public function index() : View
     {
-        return view('tasks.index');
+        $tasks = $this->taskService->getAllTasks();
+        return view('tasks.index', compact('tasks'));
     }
 
-    public function create()
+    public function store(StoreTaskRequest $request) : RedirectResponse
     {
-
-    }
-
-    public function store()
-    {
-
+        $this->taskService->storeTask($request);
+        return to_route('tasks.index');
     }
 
     public function edit()
--- resources/views/tasks/list.blade.php
+++ resources/views/tasks/list.blade.php
@@ -1,25 +1,27 @@
 <ul class="list-group">
-    <li class="list-group-item">
-        <div>
-            <input class="form-check-input" type="checkbox" name="" id="">
-            <p>Escovar os dentes</p>
-        </div>
-        <div>
-            <i class="bi bi-trash"></i>
-            <i class="bi bi-pencil-fill"></i>
-        </div>
-    </li>
-
-    <li class="list-group-item"><input class="form-check-input" type="checkbox" name="" id=""> Acordar</li>
-    <li class="list-group-item"><input class="form-check-input" type="checkbox" name="" id=""> Abracar Isabela</li>
+    @forelse($tasks as $task)
+        <li class="list-group-item d-flex justify-content-between">
+            <div>
+                <input class="form-check-input" type="checkbox" name="task" id="task">
+                <label for="task">{{$task->description}}</label>
+            </div>
+            <div class="d-flex gap-2">
+                <i class="bi bi-trash"></i>
+                <i class="bi bi-pencil-fill"></i>
+            </div>
+        </li>
+    @empty
+        <li>Add a task!</li>
+    @endforelse
 </ul>
 
 <ul class="list-group">
     <li class="list-group-item" role="button"><i class="bi bi-plus-circle-dotted"></i> Adicionar nova tarefa</li>
 </ul>
 
-<form class="p-3 d-flex gap-2">
-    <input class="form-control" type="text" placeholder="Task description...">
+<form action="{{route('tasks.store')}}" method="POST" class="p-3 d-flex gap-2">
+    @csrf
+    <input class="form-control" name="description" type="text" placeholder="Task description...">
     <button class="btn btn-outline-success">Save</button>
     <button class="btn btn-outline-danger">Cancel</button>
 </form>
