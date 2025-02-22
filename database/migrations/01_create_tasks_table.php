c875a6988a06b341c6a08213197fb769133b1c2c
IllgnerJose <illgnerjosbatista@gmail.com>
2025-02-22 14:33:39 -0300

    refactor: add default value to completed column in task table
    When i was reviewning the code i've noticed that the completed column in task table didnt has a default value. Then, i added the value false on default.

--- database/migrations/01_create_tasks_table.php
+++ database/migrations/01_create_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('description', 255);
-            $table->boolean('completed');
+            $table->boolean('completed')->default(false);
             $table->timestamps();
         });
     }
