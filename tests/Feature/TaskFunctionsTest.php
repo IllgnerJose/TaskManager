ff406f543bfe5e946778a9dde8087838a477d0c7
IllgnerJose <illgnerjosbatista@gmail.com>
2025-03-03 11:48:32 -0300

    test: implement test for reading of the task list page

--- tests/Feature/TaskFunctionsTest.php
+++ tests/Feature/TaskFunctionsTest.php
@@ -15,3 +15,8 @@ test("can create a task", function() {
         ->description->toBe($task["description"])
         ->completed->toBe(0);
 });
+
+test("can load the task list page", function () {
+    $this->get(route("tasks.index"))
+        ->assertStatus(200);
+});
